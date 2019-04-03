32afd575722272ad2891ecd18d4303b64c5d06f0
mjtedder <mjtedder@gmail.com>
2019-04-02 20:35:36 -0600

    create div component

---
+++ src/components/HelloDiv.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HelloDiv() {
+  return  (<div>
+            <h1>Hello!  My name is Billy.</h1>
+            <h3>Here are some things I like:</h3>
+              <ul>
+                <li>Music</li>
+                <li>Video Games</li>
+                <li>Travel</li>
+              </ul>
+          </div>)
+}
+
+export default HelloDiv;
