a3a2b8a1c434284e2557a9f71e4a8e75374b3c90
mjtedder <mjtedder@gmail.com>
2019-04-06 11:30:26 -0600

    Add removeVowels function

--- src/components/JSXVariables.js
+++ src/components/JSXVariables.js
@@ -4,6 +4,16 @@ import React from "react";
 const name = "Matt";
 const thoughts = "is better than HBS!"
 
+const removeVowels = str => {
+  const vowels = ['a', 'e', 'i', 'o', 'u']
+  let result = ''
+  for (let i = 0; i < str.length; i++) {
+    if (!vowels.includes(str[i].toLowerCase())) {
+      result += str[i]
+    }
+  }
+  return result
+}
 
 function JSXVariables() {
   return (
@@ -13,6 +23,7 @@ function JSXVariables() {
           <h1>Hi! My name is {name}</h1>
           <h2>My name has {name.length} letters</h2>
           <h2>I think React {thoughts}</h2>
+          <h2>My name with no vowels is {removeVowels("matt")}</h2>
         </div>
       </div>
     </div>
